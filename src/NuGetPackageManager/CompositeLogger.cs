b49db06d2576668bffd294faaabf9045f87871f5
Artak <34246760+mkArtakMSFT@users.noreply.github.com>
2020-04-01 18:04:48 -0700

    Updated to supported package versions

--- src/NuGetPackageManager/CompositeLogger.cs
+++ src/NuGetPackageManager/CompositeLogger.cs
@@ -1,48 +1,64 @@
 ﻿using NuGet.Common;
 using System;
+using System.Threading.Tasks;
 
 namespace NuGetPackageManager
 {
     internal class CompositeLogger : ILogger
     {
+        public void Log(LogLevel level, string data)
+        {
+            Console.WriteLine($"{level}:: {data}");
+        }
+
+        public void Log(ILogMessage message)
+        {
+            Console.WriteLine($"{message.Level}:: {message.Message}");
+        }
+
+        public Task LogAsync(LogLevel level, string data)
+        {
+            return Task.Run(() => Log(level, data));
+        }
+
+        public Task LogAsync(ILogMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
         public void LogDebug(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Debug, data);
         }
 
         public void LogError(string data)
         {
-            Console.WriteLine(data);
-        }
-
-        public void LogErrorSummary(string data)
-        {
-            Console.WriteLine(data);
+            Log(LogLevel.Error, data);
         }
 
         public void LogInformation(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Information, data);
         }
 
         public void LogInformationSummary(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Information, data);
         }
 
         public void LogMinimal(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Minimal, data);
         }
 
         public void LogVerbose(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Verbose, data);
         }
 
         public void LogWarning(string data)
         {
-            Console.WriteLine(data);
+            Log(LogLevel.Warning, data);
         }
     }
 }
